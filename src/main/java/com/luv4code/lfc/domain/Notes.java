32b6ea11b20b2a911305e2578eb7da15aac9525b
luv4boot <luv4boot@gmail.com>
2021-11-20 15:23:17 +0530

    adding one to one relatioanl mapping

---
+++ src/main/java/com/luv4code/lfc/domain/Notes.java
@@ -0,0 +1,24 @@
+package com.luv4code.lfc.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
---
+++ src/main/java/com/luv4code/lfc/domain/Recipe.java
@@ -0,0 +1,32 @@
+package com.luv4code.lfc.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+}
