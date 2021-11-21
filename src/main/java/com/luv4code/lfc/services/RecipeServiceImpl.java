87946a1ba36eb07efa1868af8742dd6428b3dfd1
luv4boot <luv4boot@gmail.com>
2021-11-21 11:47:43 +0530

    adding some modification on recipe

--- src/main/java/com/luv4code/lfc/services/RecipeServiceImpl.java
+++ src/main/java/com/luv4code/lfc/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.luv4code.lfc.services;
 
 import com.luv4code.lfc.domain.Recipe;
 import com.luv4code.lfc.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
 
@@ -17,6 +19,9 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
