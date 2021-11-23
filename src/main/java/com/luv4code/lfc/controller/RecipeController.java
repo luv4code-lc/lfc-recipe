e41f2d26bbb5b4c013cf045463f17cb52dd9ae97
luv4boot <luv4boot@gmail.com>
2021-11-23 08:52:09 +0530

    adding new Recipe

--- src/main/java/com/luv4code/lfc/controller/RecipeController.java
+++ src/main/java/com/luv4code/lfc/controller/RecipeController.java
@@ -1,10 +1,13 @@
 package com.luv4code.lfc.controller;
 
+import com.luv4code.lfc.commands.RecipeCommand;
 import com.luv4code.lfc.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
@@ -18,5 +21,17 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", service.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @GetMapping("/recipe/new")
+    public String newReicpe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = service.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
